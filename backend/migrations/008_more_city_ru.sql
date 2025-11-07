71f41d3f5a27a66528b971bab1cdd889297507ef
4oko4ow <cerberussoftware823@gmail.com>
2025-11-07 12:54:59 +0700

    up

---
+++ backend/migrations/008_more_city_ru.sql
@@ -0,0 +1,97 @@
+-- 008_more_city_ru.sql
+-- Russian city names for additional airports added in 007_more_airports.sql
+
+-- Spain
+UPDATE airport SET city_ru = 'Севилья' WHERE iata = 'SVQ';
+UPDATE airport SET city_ru = 'Валенсия' WHERE iata = 'VLC';
+UPDATE airport SET city_ru = 'Бильбао' WHERE iata = 'BIO';
+UPDATE airport SET city_ru = 'Сантандер' WHERE iata = 'SDR';
+UPDATE airport SET city_ru = 'Сантьяго-де-Компостела' WHERE iata = 'SCQ';
+UPDATE airport SET city_ru = 'Реус' WHERE iata = 'REU';
+UPDATE airport SET city_ru = 'Маон' WHERE iata = 'MAH';
+UPDATE airport SET city_ru = 'Херес-де-ла-Фронтера' WHERE iata = 'XRY';
+UPDATE airport SET city_ru = 'Альмерия' WHERE iata = 'LEI';
+UPDATE airport SET city_ru = 'Сан-Кристобаль-де-ла-Лагуна' WHERE iata = 'TFN';
+UPDATE airport SET city_ru = 'Гранадилья-де-Абона' WHERE iata = 'TFS';
+UPDATE airport SET city_ru = 'Лас-Пальмас' WHERE iata = 'LPA';
+UPDATE airport SET city_ru = 'Арресифе' WHERE iata = 'ACE';
+UPDATE airport SET city_ru = 'Пуэрто-дель-Росарио' WHERE iata = 'FUE';
+UPDATE airport SET city_ru = 'Санта-Крус-де-ла-Пальма' WHERE iata = 'SPC';
+
+-- Portugal
+UPDATE airport SET city_ru = 'Лиссабон' WHERE iata = 'LIS';
+UPDATE airport SET city_ru = 'Порту' WHERE iata = 'OPO';
+UPDATE airport SET city_ru = 'Фару' WHERE iata = 'FAO';
+UPDATE airport SET city_ru = 'Фуншал' WHERE iata = 'FNC';
+UPDATE airport SET city_ru = 'Понта-Делгада' WHERE iata = 'PDL';
+UPDATE airport SET city_ru = 'Терсейра' WHERE iata = 'TER';
+
+-- North America
+UPDATE airport SET city_ru = 'Атланта' WHERE iata = 'ATL';
+UPDATE airport SET city_ru = 'Чикаго' WHERE iata = 'ORD';
+UPDATE airport SET city_ru = 'Даллас' WHERE iata = 'DFW';
+UPDATE airport SET city_ru = 'Сан-Франциско' WHERE iata = 'SFO';
+UPDATE airport SET city_ru = 'Денвер' WHERE iata = 'DEN';
+UPDATE airport SET city_ru = 'Миннеаполис' WHERE iata = 'MSP';
+UPDATE airport SET city_ru = 'Детройт' WHERE iata = 'DTW';
+UPDATE airport SET city_ru = 'Лас-Вегас' WHERE iata = 'LAS';
+UPDATE airport SET city_ru = 'Ньюарк' WHERE iata = 'EWR';
+UPDATE airport SET city_ru = 'Финикс' WHERE iata = 'PHX';
+UPDATE airport SET city_ru = 'Хьюстон' WHERE iata = 'IAH';
+UPDATE airport SET city_ru = 'Сент-Луис' WHERE iata = 'STL';
+UPDATE airport SET city_ru = 'Орландо' WHERE iata = 'MCO';
+UPDATE airport SET city_ru = 'Бостон' WHERE iata = 'BOS';
+UPDATE airport SET city_ru = 'Филадельфия' WHERE iata = 'PHL';
+UPDATE airport SET city_ru = 'Нью-Йорк' WHERE iata = 'LGA';
+UPDATE airport SET city_ru = 'Цинциннати' WHERE iata = 'CVG';
+UPDATE airport SET city_ru = 'Гонолулу' WHERE iata = 'HNL';
+UPDATE airport SET city_ru = 'Шарлотт' WHERE iata = 'CLT';
+UPDATE airport SET city_ru = 'Солт-Лейк-Сити' WHERE iata = 'SLC';
+UPDATE airport SET city_ru = 'Вашингтон' WHERE iata = 'IAD';
+UPDATE airport SET city_ru = 'Тампа' WHERE iata = 'TPA';
+UPDATE airport SET city_ru = 'Сан-Диего' WHERE iata = 'SAN';
+UPDATE airport SET city_ru = 'Вашингтон' WHERE iata = 'DCA';
+UPDATE airport SET city_ru = 'Кливленд' WHERE iata = 'CLE';
+UPDATE airport SET city_ru = 'Монреаль' WHERE iata = 'YUL';
+UPDATE airport SET city_ru = 'Канкун' WHERE iata = 'CUN';
+UPDATE airport SET city_ru = 'Мехико' WHERE iata = 'MEX';
+UPDATE airport SET city_ru = 'Панама' WHERE iata = 'PTY';
+UPDATE airport SET city_ru = 'Пунта-Кана' WHERE iata = 'PUJ';
+UPDATE airport SET city_ru = 'Санто-Доминго' WHERE iata = 'SDQ';
+UPDATE airport SET city_ru = 'Гавана' WHERE iata = 'HAV';
+
+-- Europe
+UPDATE airport SET city_ru = 'Цюрих' WHERE iata = 'ZRH';
+UPDATE airport SET city_ru = 'Брюссель' WHERE iata = 'BRU';
+UPDATE airport SET city_ru = 'Манчестер' WHERE iata = 'MAN';
+UPDATE airport SET city_ru = 'Копенгаген' WHERE iata = 'CPH';
+UPDATE airport SET city_ru = 'Стокгольм' WHERE iata = 'ARN';
+UPDATE airport SET city_ru = 'Дюссельдорф' WHERE iata = 'DUS';
+
+-- Asia-Pacific
+UPDATE airport SET city_ru = 'Гонконг' WHERE iata = 'HKG';
+UPDATE airport SET city_ru = 'Осака' WHERE iata = 'KIX';
+UPDATE airport SET city_ru = 'Фукуока' WHERE iata = 'FUK';
+UPDATE airport SET city_ru = 'Саппоро' WHERE iata = 'CTS';
+UPDATE airport SET city_ru = 'Тайбэй' WHERE iata = 'TPE';
+UPDATE airport SET city_ru = 'Шанхай' WHERE iata = 'SHA';
+UPDATE airport SET city_ru = 'Брисбен' WHERE iata = 'BNE';
+UPDATE airport SET city_ru = 'Перт' WHERE iata = 'PER';
+
+-- Latin America
+UPDATE airport SET city_ru = 'Богота' WHERE iata = 'BOG';
+UPDATE airport SET city_ru = 'Сантьяго' WHERE iata = 'SCL';
+UPDATE airport SET city_ru = 'Лима' WHERE iata = 'LIM';
+UPDATE airport SET city_ru = 'Буэнос-Айрес' WHERE iata = 'EZE';
+
+-- Middle East & Africa
+UPDATE airport SET city_ru = 'Доха' WHERE iata = 'DOH';
+UPDATE airport SET city_ru = 'Лагос' WHERE iata = 'LOS';
+UPDATE airport SET city_ru = 'Аккра' WHERE iata = 'ACC';
+UPDATE airport SET city_ru = 'Йоханнесбург' WHERE iata = 'JNB';
+UPDATE airport SET city_ru = 'Кейптаун' WHERE iata = 'CPT';
+UPDATE airport SET city_ru = 'Найроби' WHERE iata = 'NBO';
+UPDATE airport SET city_ru = 'Аддис-Абеба' WHERE iata = 'ADD';
+UPDATE airport SET city_ru = 'Касабланка' WHERE iata = 'CMN';
+UPDATE airport SET city_ru = 'Марракеш' WHERE iata = 'RAK';
+
